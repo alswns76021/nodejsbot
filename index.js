339f55202dcb0026f478daba31be24e2e4b06276
alswns76021 <dhkffkql888r@gmail.com>
2020-10-25 18:40:29 +0900

    add bot state

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const byeChannelComment = "안녕히가세요.";
 
 client.on('ready', () => {
   console.log('켰다.');
-  client.user.setPresence({ game: { name: '서버 관리하는 중' }, status: 'online' })
+  client.user.setPresence({ game: { name: '서버 관리' }, status: 'online' })
 });
 
 client.on("guildMemberAdd", (member) => {
